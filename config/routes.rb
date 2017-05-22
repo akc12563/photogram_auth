ae2b4465e0395610e5ccd4621e78ebee090d72e1
Amy Collins <amy.collins@user.noreply.github.com>
2017-05-22 16:30:54 -0500

    commit #2

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,14 @@ Rails.application.routes.draw do
   get "/delete_like/:id", :controller => "likes", :action => "destroy"
   #------------------------------
 
+  # Routes for the User resource:
+
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+  #------------------------------
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
